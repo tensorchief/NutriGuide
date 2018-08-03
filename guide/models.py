236c07dd891a0ef62333d98ed2a9ee748a24d98a
silviar <silvia.b.reynolds@gmail.com>
2018-08-03 20:05:26 +0200

    add icon to cards; create card object

--- guide/models.py
+++ guide/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class TitleCard(object):
+
+    def __init__(self, title, description, colour, icon):
+        self.icon = icon
+        self.colour = colour
+        self.title = title
+        self.description = description
--- guide/views.py
+++ guide/views.py
@@ -1,7 +1,24 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from guide.models import TitleCard
+
 
 def index(request):
-    return render(request, 'guide/index.html', {'title1': 'Weekly planning', 'title2': 'Shopping list', 'title3': 'Personal settings'})
+    cards = [
+        TitleCard('Weekly planning',
+                  'View weekly nutrition plan',
+                  'danger',
+                  'fa-calendar-check-o'),
+        TitleCard('Shopping list',
+                  'Add items from nutrition plan to shopping list',
+                  'warning',
+                  'fa-shopping-cart'),
+        TitleCard('Personal settings',
+                  'Set nutrition goals',
+                  'info',
+                  'fa-heartbeat'),
+
+    ]
+    return render(request, 'guide/index.html', {'cards': cards})
 
